9d7c24debfadb1a8b4261dadff723a68408a6cf5
hotatekaoru <kaorupochi@gmail.com>
2020-02-16 16:17:46 +0900

    👍 Widgetのライスボタンをタップしてアプリが起動できるようにする
    Widgetのライスボタンがタップされた際に、MainActivityが呼び出されるようにする。それぞれのボタンをenumで管理し、PendingIntent#getActivityのrequestCodeに設定することで、どのボタンが押されたかを判別できるようにする。
    
    参考: https://developer.android.com/guide/topics/appwidgets?hl=ja

--- app/src/main/java/com/hotatekaoru/carbscounter/BasicWidget.kt
+++ app/src/main/java/com/hotatekaoru/carbscounter/BasicWidget.kt
@@ -1,14 +1,20 @@
 package com.hotatekaoru.carbscounter
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
 
 /**
  * Implementation of App Widget functionality.
  */
 class BasicWidget : AppWidgetProvider() {
+    companion object {
+        const val INTENT_ACTION_NAME = "TAP_WIDGET_BUTTON"
+    }
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
@@ -29,7 +35,17 @@ class BasicWidget : AppWidgetProvider() {
     }
 
     private fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
-        // TODO:
+
+        val views = RemoteViews(context.packageName, R.layout.basic_widget)
+        views.setOnClickPendingIntent(R.id.widget_rice_button, getPendingIntent(context, CarbType.RICE))
+
+        appWidgetManager.updateAppWidget(appWidgetId, views)
     }
 
+    private fun getPendingIntent(context: Context, type: CarbType): PendingIntent {
+
+        val resultIntent = Intent(context, MainActivity::class.java)
+        resultIntent.putExtra(PRESSED_BUTTON_TYPE, type.ordinal)
+        return PendingIntent.getActivity(context, type.ordinal, resultIntent, Intent.FILL_IN_ACTION)
+    }
 }
